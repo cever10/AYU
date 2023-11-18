f62902ef7592e3da5fc38d1461fcf2738faa0c0e
eotjd3012 <eotjd3012@naver.com>
2023-11-18 22:07:09 +0900

    헤더 수정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import React from 'react';
 import {BrowserRouter, Route,Routes} from 'react-router-dom';
-import Shortpage from './ShortsPage';
-import HomePage from './HomePage';
+import Shortpage from './pages/ShortsPage';
+import HomePage from './pages/HomePage';
 
 function App () {
   return (
@@ -11,7 +11,6 @@ function App () {
     <main>
     <Routes>
 
-        {/*네비게이션 바 여기에*/}
           <Route exact path="/" Component={HomePage}/>
           <Route path="/Short"Component={Shortpage} />
    </Routes>
---
+++ src/pages/PopularVideos.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PopularVideos = () => {
+    return(
+        <div>
+            <h2>인기 동영상</h2>
+        </div>
+    );
+};
+
+export default PopularVideos;
+
