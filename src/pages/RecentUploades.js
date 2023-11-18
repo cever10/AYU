e2c4e073444e91bf8798329975e59e01d74c3938
eotjd3012 <eotjd3012@naver.com>
2023-11-18 22:47:32 +0900

    최근 업로드 영상 컴포넌트

--- src/pages/RecentUploades.js
+++ src/pages/RecentUploades.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, {useState, useEffect}from "react";
 
 const RecentUploades = () => {
+    const [Videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        
+        fetchRecentUploads()
+        .then((data) => {
+            setVideos(data);
+        })
+        .catch((error) => {
+            console.error('Error fetching recent uploads:', error);
+        });
+    }, []);
     return(
-        <div>
+        <div className="recent-uploades">
             <h2>최근 업로드 영상</h2>
         </div>
     );
