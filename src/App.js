ff9875e31710a49b872e77f6eab064116afc90f3
cever10 <jiwan_01@naver.com>
2023-12-01 15:39:56 +0900

    잘못 병합된거 초기화용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
@@ -37,27 +36,3 @@ function App() {
 }
 
 export default App;
-=======
-import './App.css';
-import React from 'react';
-import {BrowserRouter, Route,Routes} from 'react-router-dom';
-import Shortpage from './pages/ShortsPage';
-import HomePage from './pages/HomePage';
-
-function App () {
-  return (
-    <BrowserRouter>
-    <header/>
-    <main>
-    <Routes>
-
-          <Route exact path="/" Component={HomePage}/>
-          <Route path="/Short"Component={Shortpage} />
-   </Routes>
-   </main>
-   </BrowserRouter>
-  );
-};
-
-export default App;
->>>>>>> master
